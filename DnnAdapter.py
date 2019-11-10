fe15eb6f1b60a7bd2f8f356fc3914e315505d2e6
RustamovAzer <azer.rustamov.01@gmail.com>
2019-11-10 20:45:28 +0300

    Add samples

--- DnnAdapter.py
+++ DnnAdapter.py
@@ -4,10 +4,11 @@ import numpy
 
 class DnnAdapter:
 
-    def __init__(self, model_path, weights_path=None, task_type=None):  # и другие параметры пустым значением по умолчанию
+    def __init__(self, model_path, weights_path=None, task_type=None, net = None):  # и другие параметры пустым значением по умолчанию
         self.model = model_path
         self.weights = weights_path
         self.task_type = task_type
+        self.net = cv2.dnn.readNetFromCaffe(self.model, self.weights)
 
     def process_image(self, image):
         # Read image
@@ -15,29 +16,16 @@ class DnnAdapter:
         # forward
 
         if self.task_type == 'face_detection':
-            net = cv2.dnn.readNetFromCaffe(self.model, self.weights)
-            (h, w) = img.shape[:2]
+
             blob = cv2.dnn.blobFromImage(cv2.resize(img, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
-            net.setInput(blob)
-            detections = net.forward()
-            for i in range(0, detections.shape[2]):
-                confidence = detections[0, 0, i, 2]
-                if confidence > 0.5:
-                    box = detections[0, 0, i, 3:7] * numpy.array([w, h, w, h])
-                    (startX, startY, endX, endY) = box.astype("int")
-                    text = "{:.2f}%".format(confidence * 100)
-                    y = startY - 10 if startY - 10 > 10 else startY + 10
-                    cv2.rectangle(img, (startX, startY), (endX, endY),
-                                  (0, 255, 0), 2)
-                    cv2.putText(img, text, (startX, y),
-                                cv2.FONT_HERSHEY_COMPLEX, 0.45, (0, 0, 255), 1)
-            cv2.imshow("Output", img)
-            cv2.waitKey(0)
+            self.net.setInput(blob)
+            output = self.net.forward()
+            self._output_face_detection(output, img)
+
         if self.task_type == 'classification':
             blob = cv2.dnn.blobFromImage(img, 1, (224, 224), (104, 117, 123))
-            net = cv2.dnn.readNetFromCaffe(self.model, self.weights)
-            net.setInput(blob)
-            output = net.forward()
+            self.net.setInput(blob)
+            output = self.net.forward()
             self._output_classification(output)
 
     def _output_classification(self, output):
@@ -47,3 +35,19 @@ class DnnAdapter:
         indexes = numpy.argsort(output[0])[-5:]
         for i in reversed(indexes):
             print('class:', classes[i], ' probability:', output[0][i])
+
+    def _output_face_detection(self, output, img):
+        (h, w) = img.shape[:2]
+        for i in range(0, output.shape[2]):
+            confidence = output[0, 0, i, 2]
+            if confidence > 0.5:
+                box = output[0, 0, i, 3:7] * numpy.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+                text = "{:.2f}%".format(confidence * 100)
+                y = startY - 10 if startY - 10 > 10 else startY + 10
+                cv2.rectangle(img, (startX, startY), (endX, endY),
+                              (0, 255, 0), 2)
+                cv2.putText(img, text, (startX, y),
+                            cv2.FONT_HERSHEY_COMPLEX, 0.45, (0, 0, 255), 1)
+        cv2.imshow("Output", img)
+        cv2.waitKey(0)
